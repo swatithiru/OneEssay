567ebbf7b3d6d0519881ff5f89faeeb1354eb6b4
SwatiThiru2510 <swati.thirunavakarasu@gmail.com>
2019-04-28 22:57:58 -0500

    Added Code for real time viewing.

--- app/src/main/java/com/example/oneessay/MainActivity.java
+++ app/src/main/java/com/example/oneessay/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -65,6 +66,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        if(!LoginActivity.currentUser.getEmail().equalsIgnoreCase("p@gmail.com")) {
+            Menu nav_Menu = navigationView.getMenu();
+            nav_Menu.findItem(R.id.add).setVisible(false);
+        }
+
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         actionBarDrawerToggle.syncState();
